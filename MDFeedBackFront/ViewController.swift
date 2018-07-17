30dfbf9a1f89b6bbfaad9498c45f818fbdd970c2
Андрей Васильев <adlibbeats@hotmail.com>
2018-07-17 23:13:40 +0300

    Initial Commit

---
+++ MDFeedBackFront/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MDFeedBackFront
+//
+//  Created by Андрей Васильев on 17.07.2018.
+//  Copyright © 2018 Андрей Васильев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
